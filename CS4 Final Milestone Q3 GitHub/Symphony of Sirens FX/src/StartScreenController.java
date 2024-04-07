5ccf80ab9f2bebddc6af9561a9a9d2630c06eddc
thfdo <156043114+thfdo@users.noreply.github.com>
2024-04-07 12:51:06 +0800

    Update StartScreenController.java

--- CS4 Final Milestone Q3 GitHub/Symphony of Sirens FX/src/StartScreenController.java
+++ CS4 Final Milestone Q3 GitHub/Symphony of Sirens FX/src/StartScreenController.java
@@ -1,25 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Shawn
- */
 public class StartScreenController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button playButton;
+    @FXML
+    private Button shopButton;
+    @FXML
+    private Button stageSelectorButton;
+    @FXML
+    private Button quitButton;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        // perform any initialization tasks here.
+    }
+
+    @FXML
+    private void handlePlayButtonClicked(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GameScreen.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) playButton.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handleShopButtonClicked(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Shop.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) shopButton.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handleStageSelectorButtonClicked(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("StageSelector.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) stageSelectorButton.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handleQuitButtonClicked(ActionEvent event) {
+        Stage stage = (Stage) quitButton.getScene().getWindow();
+        stage.close();
+    }
 }
